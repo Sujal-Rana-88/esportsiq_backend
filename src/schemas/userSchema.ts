641e251a4279aee0ef4feeb7a859a436c74c3e28
Sujal Rana <118067837+Sujal-Rana-88@users.noreply.github.com>
2025-03-22 11:21:06 +0530

    user shcema updated

--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -9,7 +9,18 @@ const userSchema = `
     isActive BOOLEAN DEFAULT TRUE,
     profilePictureUrl TEXT,
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    rating FLOAT DEFAULT 0,
+    ratingCount INT DEFAULT 0,
+    isVerified BOOLEAN DEFAULT FALSE,
+    provider VARCHAR(50) NOT NULL DEFAULT 'local',
+    providerId VARCHAR(255) NOT NULL DEFAULT '',
+    accessToken TEXT,
+    refreshToken TEXT,
+    verifyToken VARCHAR(6) NULL, -- Stores OTP for email verification
+    verifyTokenExpiry TIMESTAMP NULL, -- Expiration time of OTP
+    passwordResetToken VARCHAR(255) NULL, -- Stores token for password reset
+    passwordResetExpiry TIMESTAMP NULL -- Expiration time for password reset token
   );
 `;
 
